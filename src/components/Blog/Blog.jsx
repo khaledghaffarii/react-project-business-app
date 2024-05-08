256e472944df94de8f741bc6d0b3641e4ce9a0c4
khaledghaffarii <75438046+khaledghaffarii@users.noreply.github.com>
2024-05-08 13:45:25 +0100

    UPDATE

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
-import './Blog.css';
-import BlogImage from '../../assets/blog/blog.jpg';
+import React from "react";
+import "./Blog.css";
+import BlogImage from "../../assets/blog/blog2.jpeg";
 
 export default function Blog() {
   return (
     <>
-    <section id="blog">
+      <section id="blog">
         <div className="container">
-            <div className="blog_wrapper">
-                <div className="blog_col">
-                    <h3>WE HELP BUSINESSES LAUNCH, GROW AND SUCCEED</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, corrupti odit? At iure facere, porro repellat officia quas, dolores magnam assumenda soluta odit harum voluptate inventore ipsa maiores fugiat accusamus eos nulla. Iure voluptatibus explicabo officia.</p>
-                    <div className="btn_wrapper">
-                        <a className="btn" href="/">Get Started</a>
-                    </div>
-                </div>
-                <div className="blog_col">
-                    <div className="blog_image">
-                        <img src={BlogImage} alt="Blog" />
-                    </div>
-                </div>
+          <div className="blog_wrapper">
+            <div className="blog_col">
+              <h3>WE HELP BUSINESSES LAUNCH, GROW AND SUCCEED</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. At,
+                corrupti odit? At iure facere, porro repellat officia quas,
+                dolores magnam assumenda soluta odit harum voluptate inventore
+                ipsa maiores fugiat accusamus eos nulla. Iure voluptatibus
+                explicabo officia.
+              </p>
+              <div className="btn_wrapper">
+                <a className="btn" href="/">
+                  Get Started
+                </a>
+              </div>
             </div>
+            <div className="blog_col">
+              <div className="blog_image">
+                <img
+                  style={{ height: 400, width: 700 }}
+                  src={BlogImage}
+                  alt="Blog"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
